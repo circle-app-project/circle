db68573a23bc2cb9d70cdd7e8ee691a303975204
Lewis <lewisnuikweh@gmail.com>
2024-11-15 12:21:03 +0100

    database: reimplemented water local service with object box

--- lib/features/water/services/local/water_local_service.dart
+++ lib/features/water/services/local/water_local_service.dart
@@ -1,59 +1,89 @@
 import '../../../../databse_service.dart';
+import '../../../../objectbox.g.dart';
 import '../../water.dart';
 
-class WaterLocalService extends LocalDbService {
-  WaterLocalService();
+class WaterLocalService {
+  late final Box<WaterLog> waterLogBox;
+  late final Box<WaterPreferences> waterPreferencesBox;
+
+  WaterLocalService({required Store store})
+      : waterLogBox = store.box<WaterLog>(),
+        waterPreferencesBox = store.box<WaterPreferences>();
 
   ///----Water Logs ----///
   Future<List<WaterLog>> getWaterLogs({DateTime? start, DateTime? end}) async {
-    ///Todo: Implement
-    throw UnimplementedError();
+    List<WaterLog> logs = [];
+    Query query;
+    if (start != null && end != null) {
+      query = waterLogBox
+          .query(WaterLog_.timestamp.betweenDate(start, end))
+          .build();
+      logs = query.find() as List<WaterLog>;
+      query.close();
+    }
+
+    if (start != null && end == null) {
+      query = waterLogBox
+          .query(WaterLog_.timestamp.greaterOrEqualDate(start))
+          .build();
+      logs = query.find() as List<WaterLog>;
+      query.close();
+    }
+
+    if (end != null && start == null) {
+      query =
+          waterLogBox.query(WaterLog_.timestamp.lessOrEqualDate(end)).build();
+      logs = query.find() as List<WaterLog>;
+      query.close();
+    }
+
+    if (start == null && end == null) {
+      logs = waterLogBox.getAll();
+    }
+
+    logs.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+    return logs;
   }
 
-  Future<void> addWaterLog(WaterLog log) async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void addWaterLog(WaterLog log) {
+    waterLogBox.put(log);
   }
 
-  Future<void> update(WaterLog log) async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void update(WaterLog log) {
+    waterLogBox.put(log, mode: PutMode.update);
   }
 
-  Future<void> deleteWaterLog(WaterLog log) async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void deleteWaterLog(WaterLog log) {
+    bool isDeleted = waterLogBox.remove(log.id);
+
+    if (!isDeleted) {
+      throw Exception("Failed to delete log");
+    }
   }
 
-  Future<void> clear() async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void clear() {
+    waterLogBox.removeAll();
   }
 
-  Future<int> count() async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  int count() {
+    return waterLogBox.count();
   }
 
   /// ----- Water Preferences Section ----- ///
 
-  Future<WaterPreferences> getPreferences() async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  WaterPreferences getPreferences() {
+    return waterPreferencesBox.getAll().first;
   }
 
-  Future<void> addPreferences(WaterPreferences preferences) async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void addPreferences(WaterPreferences preferences) {
+    waterPreferencesBox.put(preferences);
   }
 
-  Future<void> updatePreferences(WaterPreferences preferences) async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void updatePreferences(WaterPreferences preferences) {
+    waterPreferencesBox.put(preferences, mode: PutMode.update);
   }
 
-  Future<void> deletePreferences() async {
-    ///Todo: Implement
-    throw UnimplementedError();
+  void deletePreferences() {
+    waterPreferencesBox.removeAll();
   }
 }
