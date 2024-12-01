de25b151becb4d8627612903122def9d1eeaba00
Lewis <lewisnuikweh@gmail.com>
2024-12-01 23:53:57 +0100

    database: rewriting database method implementation
    Properly implemented database method implementations, to ensure data is properly stored, and updated

--- lib/features/auth/providers/user/user_notifier.dart
+++ lib/features/auth/providers/user/user_notifier.dart
@@ -34,43 +34,50 @@ class UserNotifier extends AsyncNotifier<AppUser> {
 
   Future<void> getCurrentUserData({bool forceRefresh = false}) async {
     state = const AsyncValue.loading();
-
     final Either<Failure, AppUser> response =
         await _userRepository.getCurrentUserData(forceRefresh: forceRefresh);
     response.fold((failure) {
-      state = AsyncValue.error(failure, StackTrace.current);
+      state = AsyncValue.error(failure, failure.stackTrace!);
     }, (user) {
       state = AsyncValue.data(user);
     });
   }
 
-  Future<void> addUserData(
+  /// Puts User Data
+  /// If data exists, it will be updated, if not, it will be added and the updated data with the new id from object box will be returned and set to stata
+  Future<void> putUserData(
       {required AppUser user, bool updateRemote = false}) async {
-    log("ADDING DATA");
+   // log("ADDING USER DATA TO LOCAL DB", name: "USER NOTIFIER");
+    log("PUTTING USER DATA TO LOCAL DB", name: "USER NOTIFIER");
     state = const AsyncValue.loading();
-    final Either<Failure, void> response = await _userRepository.addUserData(
+    final Either<Failure, AppUser> response = await _userRepository.addUserData(
         user: user, updateRemote: updateRemote);
     response.fold((failure) {
-      log("RETURNED FAILURE");
-      state = AsyncValue.error(failure, StackTrace.current);
-    }, (empty) {
-      log("RETURNED SUCCESS");
+      log("RETURNED FAILURE", name: "USER NOTIFIER", stackTrace: failure.stackTrace);
+      state = AsyncValue.error(failure, failure.stackTrace!);
+    }, (user) {
+
+      log("RETURNED SUCCESS", name: "USER NOTIFIER");
+      print("verifying retruned id after putting data is actually updated");
+
+      print( user.id);
       //Set the state to be equal to the data that was just added
       state = AsyncValue.data(user);
     });
   }
 
+  @Deprecated("Prefer putUserData instead")
   Future<void> updateUserData(
       {required AppUser user, bool updateRemote = false}) async {
-    log("UPDATING USER DATA");
+    log("Updating User Data", name: "USER NOTIFIER");
     state = const AsyncValue.loading();
-    final Either<Failure, void> response = await _userRepository.updateUserData(
+    final Either<Failure, AppUser> response = await _userRepository.updateUserData(
         user: user, updateRemote: updateRemote);
     response.fold((failure) {
-      log("RETURNED FAILURE");
-      state = AsyncValue.error(failure, StackTrace.current);
-    }, (empty) {
-      log("RETURNED SUCCESS");
+      log("RETURNED FAILURE", name: "USER NOTIFIER", stackTrace: failure.stackTrace);
+      state = AsyncValue.error(failure, failure.stackTrace!);
+    }, (user) {
+      log("RETURNED SUCCESS", name: "USER NOTIFIER");
       //Set the state to be equal to the data that was just added
       state = AsyncValue.data(user);
     });
@@ -82,7 +89,8 @@ class UserNotifier extends AsyncNotifier<AppUser> {
     final Either<Failure, void> response = await _userRepository.deleteUserData(
         user: user, deleteRemote: updateRemote);
     response.fold((failure) {
-      state = AsyncValue.error(failure, StackTrace.current);
+      log("RETURNED FAILURE", name: "USER NOTIFIER", stackTrace: failure.stackTrace);
+      state = AsyncValue.error(failure, failure.stackTrace!);
     }, (empty) {
       state = AsyncValue.data(AppUser.empty);
     });
--- lib/features/auth/services/user/local/user_local_service.dart
+++ lib/features/auth/services/user/local/user_local_service.dart
@@ -1,4 +1,3 @@
-
 import 'package:circle/objectbox.g.dart';
 
 import '../../../auth.dart';
@@ -8,10 +7,12 @@ import '../../../auth.dart';
 class UserLocalService {
   late final Box<AppUser> _userBox;
   late final Box<UserPreferences> _userPreferencesBox;
+  late final Box<UserProfile> _userProfileBox;
 
   UserLocalService({required Store store})
-      : _userBox = store.box<AppUser>(),
-        _userPreferencesBox = store.box<UserPreferences>();
+    : _userBox = store.box<AppUser>(),
+      _userPreferencesBox = store.box<UserPreferences>(),
+      _userProfileBox = store.box<UserProfile>();
 
   Stream<UserPreferences> listenUserPreferences() {
     return _userPreferencesBox
@@ -29,29 +30,64 @@ class UserLocalService {
     }
   }
 
-  AppUser getUserByUid(String uid) {
+  AppUser? getUserByUid(String uid) {
+    /// Attempts to get a user with this uid, returns null if a user doesn't exits
     Query query = _userBox.query(AppUser_.uid.equals(uid)).build();
-    AppUser user = query.find().first;
-    query.close();
-    return user;
+    try {
+      AppUser user = query.find().first;
+      query.close();
+      return user;
+    } catch (e) {
+      query.close();
+      return null;
+    }
   }
 
   List<AppUser> getUsers() {
     return _userBox.getAll();
   }
 
-  void addUser(AppUser user) {
-    _userBox.put(user);
+  Future<AppUser> putAndGetUser(AppUser user) async {
+    // Save relations first
+    if (user.profile.target != null) {
+      user.profile.target = await _userProfileBox.putAndGetAsync(
+        user.profile.target!,
+        mode: user.profile.target?.id == 0 ? PutMode.put : PutMode.update,
+      );
+    }
+    if (user.preferences.target != null) {
+      user.preferences.target = await _userPreferencesBox.putAndGetAsync(
+        user.preferences.target!,
+        mode: user.preferences.target?.id == 0 ? PutMode.put : PutMode.update,
+      );
+    }
+
+    // Then save user
+    return _userBox.putAndGetAsync(
+      user,
+      mode: user.id == 0 ? PutMode.put : PutMode.update,
+    );
   }
 
-  void updateUser(AppUser user) {
-    _userBox.put(user, mode: PutMode.update);
+  @Deprecated("Prefer PutAndGetUser instead")
+  int updateUser(AppUser user) {
+    return _userBox.put(user, mode: PutMode.update);
   }
 
   void deleteUser({AppUser? user}) {
+
     if (user == null) {
+      _userProfileBox.removeAll();
+      _userPreferencesBox.removeAll();
       _userBox.removeAll();
     } else {
+      if(user.profile.target?.id != null){
+        _userProfileBox.remove(user.profile.target!.id);
+      }
+
+      if(user.preferences.target?.id != null){
+        _userPreferencesBox.remove(user.preferences.target!.id);
+      }
       _userBox.remove(user.id);
     }
   }
