e11aa962b28fcd06366a8381ef776f6d18a43140
Lewis <lewisnuikweh@gmail.com>
2024-12-01 23:53:57 +0100

    database: rewriting database method implementation
    Properly implemented database method implementations, to ensure data is properly stored, and updated

--- lib/features/auth/repositories/user/user_repository.dart
+++ lib/features/auth/repositories/user/user_repository.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fpdart/fpdart.dart';
 import '../../../../core/core.dart';
 import '../../auth.dart';
 import '../../services/auth/remote/auth_service.dart';
@@ -13,16 +14,15 @@ class UserRepository {
   final UserLocalService _userLocalService;
   final AuthService _authService;
 
-  UserRepository(
-      {required UserService userService,
-      required UserLocalService userLocalService,
-      required AuthService authService})
-      : _authService = authService,
-        _userLocalService = userLocalService,
-        _userService = userService;
+  UserRepository({
+    required UserService userService,
+    required UserLocalService userLocalService,
+    required AuthService authService,
+  }) : _authService = authService,
+       _userLocalService = userLocalService,
+       _userService = userService;
 
-  FutureEither<AppUser> getCurrentUserData(
-      {bool forceRefresh = false}) async {
+  FutureEither<AppUser> getCurrentUserData({bool forceRefresh = false}) async {
     return futureHandler(() async {
       ///First get from Firebase Auth in order to verify if the user is actually signed in before making any calls to remote;
 
@@ -30,70 +30,74 @@ class UserRepository {
 
       if (currentUser == null) {
         ///If user is not signed in, return an empty class
-        log("User is not signed in");
+        log("USER IS NOT SIGNED IN", name: "FIREBASE AUTH");
         _userLocalService.deleteUser();
         return AppUser.empty;
       }
 
       if (forceRefresh) {
-        log("Force refresh from remote");
+        log("FORCE REFRESHING FROM REMOTE", name: "USER REPOSITORY");
         return await _getRemoteUser(currentUser.uid);
       }
 
       AppUser localUser = _userLocalService.getUser();
       if (localUser.isNotEmpty) {
-        log("Local User Found");
+        log("LOCAL USER FOUND", name: "USER LOCAL SERVICE");
         return localUser;
       } else {
-        log("Local user is empty");
+        log("LOCAL USER IS EMPTY", name: "USER LOCAL SERVICE");
         _userLocalService.deleteUser();
         return AppUser.empty;
       }
-
     });
   }
 
   Future<AppUser> _getRemoteUser(String uid) async {
-    DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
-        await _userService.getUserData(uid);
+    DocumentSnapshot<Map<String, dynamic>> documentSnapshot = await _userService
+        .getUserData(uid);
 
     if (documentSnapshot.exists &&
         documentSnapshot.data() != null &&
         documentSnapshot.data()!.isNotEmpty) {
-      AppUser remoteUser =
-          AppUser.fromMap(data: documentSnapshot.data()!);
-      _userLocalService.addUser(remoteUser);
+      AppUser remoteUser = AppUser.fromMap(data: documentSnapshot.data()!);
+      _userLocalService.putAndGetUser(remoteUser);
       return remoteUser;
     } else {
       throw Exception("User data doesn't exist");
     }
   }
 
-  FutureEither<void> updateUserData(
-      {required AppUser user, bool updateRemote = false}) async {
+  FutureEither<AppUser> updateUserData({
+    required AppUser user,
+    bool updateRemote = false,
+  }) async {
     return futureHandler(() async {
-      _userLocalService.updateUser(user);
       if (updateRemote) {
         await _userService.updateUserData(user);
       }
+      return await _userLocalService.putAndGetUser(user);
     });
   }
 
-  FutureEither<void> addUserData(
-      {required AppUser user, bool updateRemote = false}) async {
+  FutureEither<AppUser> addUserData({
+    required AppUser user,
+    bool updateRemote = false,
+  }) async {
     return futureHandler(() async {
-       _userLocalService.updateUser(user);
       if (updateRemote) {
         await _userService.addUserData(user);
       }
+      return await _userLocalService.putAndGetUser(user);
     });
   }
 
   /// --------- Delete User Data --------///
-  FutureEither<void> deleteUserData(
-      {required AppUser user, bool deleteRemote = false}) async {
+  FutureEither<void> deleteUserData({
+    required AppUser user,
+    bool deleteRemote = false,
+  }) async {
     return futureHandler(() async {
-     _userLocalService.deleteUser(user: user);
+      _userLocalService.deleteUser(user: user);
 
       if (deleteRemote) {
         await _userService.deleteUserData(user.uid);
