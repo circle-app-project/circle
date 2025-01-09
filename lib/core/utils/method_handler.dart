acc8206629a1ef5c2e0eaedff8fbd8d88d64f75a
Lewis <lewisnuikweh@gmail.com>
2025-01-09 02:41:47 +0100

    exception: added appexception class for more granular error handling

--- lib/core/utils/method_handler.dart
+++ lib/core/utils/method_handler.dart
@@ -1,13 +1,42 @@
 import 'dart:developer';
-
+import 'package:circle/core/error/exceptions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fpdart/fpdart.dart';
 
 import '../../crashytics_service.dart';
 import '../core.dart';
 
-///A Try catch Wrapper for [Future<Either, T>]. Evaluates the function, handles
-///errors and returns Left or Right.
+/// A utility function to handle asynchronous operations that return a
+/// [FutureEither<T>] or [Either<Failure, T>].
+///
+/// This function wraps the provided asynchronous [function] call in a try-catch block,
+/// automatically handles exceptions, and returns a [Right] if successful or a [Left]
+/// containing the appropriate [Failure] if an error occurs.
+///
+/// #### Error Handling:
+/// - Handles [AppException] types and maps them to specific [Failure] types based on
+///   the `ExceptionType` (e.g., `api`, `network`, `localStorage`, `generic`).
+/// - Logs all exceptions and their stack traces using the Dart [log] utility.
+/// - Provides a place for integrating error reporting (e.g., Crashlytics).
+///
+/// #### Example:
+/// ```dart
+/// final FutureEither<T> result = await futureHandler(() async {
+///   return await fetchUserData();
+/// });
+/// result.fold(
+///   (failure) => handleFailure(failure),
+///   (data) => processData(data),
+/// );
+/// ```
+///
+/// #### Parameters:
+/// - [function]: A [Future]-returning function that will be executed and evaluated.
+///
+/// #### Returns:
+/// - A [FutureEither] with itself is a type def of [Future<Either<Failure, T>>] containing:
+///   - [Right] if the operation succeeds, wrapping the result of type [T].
+///   - [Left] if an exception is caught, wrapping a [Failure] instance.
 FutureEither<T> futureHandler<T>(Future<T> Function() function) async {
   try {
     T result = await function();
@@ -15,8 +44,8 @@ FutureEither<T> futureHandler<T>(Future<T> Function() function) async {
   } on FirebaseException catch (e, stackTrace) {
     // A Firebase Exception has occurred
     log(
-      "A Firebase exception has occurred",
-      name: "FIREBASE",
+      "A Firebase exception has occurred: ${e.message}",
+      name: "Future Handler",
       stackTrace: stackTrace,
       error: e,
     );
@@ -36,13 +65,53 @@ FutureEither<T> futureHandler<T>(Future<T> Function() function) async {
       ],
     );
     return Left(Failure.firebase(message: e.message, stackTrace: stackTrace));
+  } on AppException catch (e, stackTrace) {
+    log(
+      "An app exception has occurred: ${e.message}",
+      error: e,
+      stackTrace: stackTrace,
+      name: "Method Handler",
+    );
+    switch (e.type) {
+      case ExceptionType.api:
+        return Left(Failure.fromApi(e));
+      case ExceptionType.network:
+        return Left(
+          Failure.network(
+            message: e.message,
+            code: e.code,
+            stackTrace: e.stackTrace,
+          ),
+        );
+      case ExceptionType.localStorage:
+        return Left(
+          Failure.storage(
+            message: e.message,
+            code: e.code,
+            stackTrace: e.stackTrace,
+          ),
+        );
+      case ExceptionType.generic:
+        return Left(
+          Failure.generic(
+            message: e.message,
+            code: e.code,
+            stackTrace: e.stackTrace,
+          ),
+        );
+
+      case ExceptionType.firebase:
+        return Left(
+          Failure.firebase(message: e.message, stackTrace: e.stackTrace),
+        );
+    }
   } catch (e, stackTrace) {
     // An exception has occurred;
     log(
       "An exception exception has occurred",
       error: e,
       stackTrace: stackTrace,
-      name: "FUTURE HANDLER",
+      name: "Future Handler",
     );
 
     // Log this caught exception to Crashlytics
@@ -50,17 +119,45 @@ FutureEither<T> futureHandler<T>(Future<T> Function() function) async {
       error: e,
       stack: stackTrace,
       reason: "An exception exception has occurred",
-      information: [
-        "Future Handler",
-        "Generic Error",
-        e.toString()
-      ],
+      information: ["Future Handler", "Generic Error", e.toString()],
     );
 
     return Left(Failure.generic(message: e.toString(), stackTrace: stackTrace));
   }
 }
 
+
+/// A utility function to handle synchronous operations that return an
+/// [Either<Failure, T>].
+///
+/// This function wraps the provided synchronous [function] call in a try-catch block,
+/// automatically handles exceptions, and returns a [Right] if successful or a [Left]
+/// containing the appropriate [Failure] if an error occurs.
+///
+/// #### Error Handling:
+/// - Handles [AppException] types and maps them to specific [Failure] types based on
+///   the `ExceptionType` (e.g., `api`, `network`, `localStorage`, `generic`).
+/// - Logs all exceptions and their stack traces using the Dart [log] utility.
+/// - Provides a place for integrating error reporting (e.g., Crashlytics).
+///
+/// #### Example:
+/// ```dart
+/// final Either<Failure, T> result = methodHandler(() {
+///   return calculateSum(10, 20);
+/// });
+/// result.fold(
+///   (failure) => handleFailure(failure),
+///   (data) => processResult(data),
+/// );
+/// ```
+///
+/// #### Parameters:
+/// - [function]: A synchronous function that will be executed and evaluated.
+///
+/// #### Returns:
+/// - An [Either] containing:
+///   - [Right] if the operation succeeds, wrapping the result of type [T].
+///   - [Left] if an exception is caught, wrapping a [Failure] instance.
 Either<Failure, T> methodHandler<T>(T Function() function) {
   try {
     T result = function();
@@ -68,20 +165,67 @@ Either<Failure, T> methodHandler<T>(T Function() function) {
   } on FirebaseException catch (e, stackTrace) {
     // A Firebase Exception has occurred
     log(
-      "A Firebase exception has occurred",
-      name: "FIREBASE",
+      "A Firebase exception has occurred: ${e.message}",
+      name: "Method Handler",
       stackTrace: stackTrace,
       error: e,
     );
-    return Left(Failure.firebase(message: e.message, stackTrace: stackTrace));
+    return Left(Failure.firebase(message: e.message, code: int.tryParse(e.code), stackTrace: stackTrace));
+  } on AppException catch (e, stackTrace) {
+    log(
+      "An app exception has occurred: ${e.message}",
+      error: e,
+      stackTrace: stackTrace,
+      name: "Method Handler",
+    );
+    switch (e.type) {
+      case ExceptionType.api:
+        return Left(Failure.fromApi(e));
+      case ExceptionType.network:
+        return Left(
+          Failure.network(
+            message: e.message,
+            code: e.code,
+            stackTrace: e.stackTrace,
+          ),
+        );
+      case ExceptionType.localStorage:
+        return Left(
+          Failure.storage(
+            message: e.message,
+            code: e.code,
+            stackTrace: e.stackTrace,
+          ),
+        );
+      case ExceptionType.generic:
+        return Left(
+          Failure.generic(
+            message: e.message,
+            code: e.code,
+            stackTrace: e.stackTrace,
+          ),
+        );
+
+      case ExceptionType.firebase:
+        return Left(
+          Failure.firebase(message: e.message, code: e.code, stackTrace: e.stackTrace),
+        );
+    }
   } catch (e, stackTrace) {
     // An exception has occurred;
     log(
       "An exception exception has occurred",
       error: e,
       stackTrace: stackTrace,
-      name: "FUTURE HANDLER",
+      name: "Method Handler",
     );
-    return Left(Failure.generic(message: e.toString(), stackTrace: stackTrace));
+    // Log this caught exception to Crashlytics
+    CrashlyticsService.recordError(
+      error: e,
+      stack: stackTrace,
+      reason: "An exception exception has occurred",
+      information: ["Method Handler", "Generic Error", e.toString()],
+    );
+    return Left(Failure.generic(message: e.toString(),  stackTrace: stackTrace));
   }
 }
