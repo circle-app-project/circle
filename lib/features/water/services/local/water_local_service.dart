c4bb714832dfdb5de18a8a3ab921c6fc98367aca
Lewis <lewisnuikweh@gmail.com>
2025-01-21 14:06:08 +0100

    water: properly rewrite water local service to handle exceptions
    - Refactored water local service to handle exceptions and provide putAndGet methods.
    - Added exception throwing for more granular error handling.
    - Added clearLogs and deletePreferences methods to waterLocalService.
    - Added putAndGetWaterLog and putAndGetPreferences methods to waterLocalService.

--- lib/features/water/services/local/water_local_service.dart
+++ lib/features/water/services/local/water_local_service.dart
@@ -1,67 +1,135 @@
+import 'dart:developer';
+
+import '../../../../core/error/exceptions.dart';
 import '../../../../objectbox.g.dart';
 import '../../water.dart';
 
+/// Todo: Add try catches to all blocks and thow App exceptions of type localdbStorage when necessary
+/// Todo: Upddate service with Put and Get
 class WaterLocalService {
   late final Box<WaterLog> waterLogBox;
   late final Box<WaterPreferences> waterPreferencesBox;
 
   WaterLocalService({required Store store})
-      : waterLogBox = store.box<WaterLog>(),
-        waterPreferencesBox = store.box<WaterPreferences>();
+    : waterLogBox = store.box<WaterLog>(),
+      waterPreferencesBox = store.box<WaterPreferences>();
 
   ///----Water Logs ----///
   Future<List<WaterLog>> getWaterLogs({DateTime? start, DateTime? end}) async {
     List<WaterLog> logs = [];
-    Query query;
-    if (start != null && end != null) {
-      query = waterLogBox
-          .query(WaterLog_.timestamp.betweenDate(start, end))
-          .build();
-      logs = query.find() as List<WaterLog>;
+    late final Query query;
+
+    try {
+      if (start != null && end != null) {
+        query =
+            waterLogBox
+                .query(WaterLog_.timestamp.betweenDate(start, end))
+                .build();
+        logs = query.find() as List<WaterLog>;
+      }
+
+      if (start != null && end == null) {
+        query =
+            waterLogBox
+                .query(WaterLog_.timestamp.greaterOrEqualDate(start))
+                .build();
+        logs = query.find() as List<WaterLog>;
+      }
+
+      if (end != null && start == null) {
+        query =
+            waterLogBox.query(WaterLog_.timestamp.lessOrEqualDate(end)).build();
+        logs = query.find() as List<WaterLog>;
+      }
+
+      if (start == null && end == null) {
+        logs = waterLogBox.getAll();
+      }
+
+      logs.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+      return logs;
+    } catch (e, stack) {
+      log(
+        "Failed to get water logs",
+        error: e,
+        stackTrace: stack,
+        name: "Water Local Service",
+      );
+      throw AppException(
+        message: "Failed to get water logs",
+        debugMessage: e.toString(),
+        stackTrace: stack,
+        type: ExceptionType.localStorage,
+      );
+    } finally {
       query.close();
     }
-
-    if (start != null && end == null) {
-      query = waterLogBox
-          .query(WaterLog_.timestamp.greaterOrEqualDate(start))
-          .build();
-      logs = query.find() as List<WaterLog>;
-      query.close();
-    }
-
-    if (end != null && start == null) {
-      query =
-          waterLogBox.query(WaterLog_.timestamp.lessOrEqualDate(end)).build();
-      logs = query.find() as List<WaterLog>;
-      query.close();
-    }
-
-    if (start == null && end == null) {
-      logs = waterLogBox.getAll();
-    }
-
-    logs.sort((a, b) => a.timestamp.compareTo(b.timestamp));
-    return logs;
   }
 
-  void addWaterLog(WaterLog log) {
-    waterLogBox.put(log);
-  }
-
-  void update(WaterLog log) {
-    waterLogBox.put(log, mode: PutMode.update);
-  }
-
-  void deleteWaterLog(WaterLog log) {
-    bool isDeleted = waterLogBox.remove(log.id);
-
-    if (!isDeleted) {
-      throw Exception("Failed to delete log");
+  Future<WaterLog> putAndGetWaterLog(WaterLog waterLog) async {
+    try {
+      return await waterLogBox.putAndGetAsync(
+        waterLog,
+        mode: waterLog.id == 0 ? PutMode.put : PutMode.update,
+      );
+    } catch (e, stackTrace) {
+      // Throws app exception
+      log(
+        "Failed to put and get water log",
+        error: e,
+        stackTrace: stackTrace,
+        name: "Medication Local Service",
+      );
+      throw AppException(
+        message: "Failed to put and get water log",
+        debugMessage: e.toString(),
+        stackTrace: stackTrace,
+        type: ExceptionType.localStorage,
+      );
     }
   }
 
-  void clear() {
-    waterLogBox.removeAll();
+  void deleteWaterLog(WaterLog waterLog) {
+    try {
+      bool isDeleted = waterLogBox.remove(waterLog.id);
+      if (isDeleted) {
+        log("WaterLog deleted successfully", name: "Water Local Service");
+      }
+    } catch (e, stackTrace) {
+      /// Log and throw app exception
+      log(
+        "Failed to delete water log",
+        error: e,
+        stackTrace: stackTrace,
+        name: "Water Local Service",
+      );
+      throw AppException(
+        message: "Failed to delete water log",
+        debugMessage: e.toString(),
+        stackTrace: stackTrace,
+        type: ExceptionType.localStorage,
+      );
+    }
+  }
+
+  void clearLogs() {
+    try {
+      waterLogBox.removeAll();
+    } catch (e, stackTrace) {
+      /// Log and throw app exception
+      log(
+        "Failed to clear logs",
+        error: e,
+        stackTrace: stackTrace,
+        name: "Water Local Service",
+      );
+      throw AppException(
+        message: "Failed to clear logs",
+        debugMessage: e.toString(),
+        stackTrace: stackTrace,
+        type: ExceptionType.localStorage,
+      );
+    }
   }
 
   int count() {
@@ -71,18 +139,75 @@ class WaterLocalService {
   /// ----- Water Preferences Section ----- ///
 
   WaterPreferences getPreferences() {
-    return waterPreferencesBox.getAll().first;
+    try {
+      List<WaterPreferences> preferences = waterPreferencesBox.getAll();
+      return preferences.isNotEmpty
+          ? preferences.first
+          : WaterPreferences.empty;
+    } catch (e, stackTrace) {
+      /// Log and throw app exception
+      log(
+        "Failed to get water preferences",
+        error: e,
+        stackTrace: stackTrace,
+        name: "Water Local Service",
+      );
+      throw AppException(
+        message: "Failed to get water preferences",
+        debugMessage: e.toString(),
+        stackTrace: stackTrace,
+        type: ExceptionType.localStorage,
+      );
+    }
   }
 
-  void addPreferences(WaterPreferences preferences) {
-    waterPreferencesBox.put(preferences);
+  Future<WaterPreferences> putAndGetPreferences(
+    WaterPreferences preferences,
+  ) async {
+    try {
+      return await waterPreferencesBox.putAndGetAsync(
+        preferences,
+        mode: preferences.id == 0 ? PutMode.put : PutMode.update,
+      );
+    } catch (e, stackTrace) {
+      // Throws app exception
+      log(
+        "Failed to put and get water preferences",
+        error: e,
+        stackTrace: stackTrace,
+        name: "Water Local Service",
+      );
+      throw AppException(
+        message: "Failed to put and get water preferences",
+        debugMessage: e.toString(),
+        stackTrace: stackTrace,
+        type: ExceptionType.localStorage,
+      );
+    }
   }
 
+  @Deprecated("Prefer putAndGetPreferences Instead")
   void updatePreferences(WaterPreferences preferences) {
     waterPreferencesBox.put(preferences, mode: PutMode.update);
   }
 
   void deletePreferences() {
-    waterPreferencesBox.removeAll();
+    try {
+      waterPreferencesBox.removeAll();
+    } catch (e, stackTrace) {
+      /// Log and throw app exception
+      log(
+        "Failed to clear water preferences",
+        error: e,
+        stackTrace: stackTrace,
+        name: "Water Local Service",
+      );
+      throw AppException(
+        message: "Failed to clear water preferences",
+        debugMessage: e.toString(),
+        stackTrace: stackTrace,
+        type: ExceptionType.localStorage,
+      );
+    }
   }
 }
