311f39140bbed07f635b9b9302fa149d908450ed
Lewis <lewisnuikweh@gmail.com>
2025-01-23 11:13:49 +0100

    meds: introduce ActivityRecord model
    Created a new abstract class `ActivityRecord` to represent medication and
    water intake activities and can be extended for any other kind of activity.
    It includes properties for date, completion status, note,
    skip reason, and activity details.

---
+++ lib/features/meds/models/activity_record.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+enum ActivityType { medication, water }
+
+enum CompletionsStatus { pending, completed, missed, skipped }
+// ignore: must_be_immutable
+abstract class ActivityRecord extends Equatable {
+  final DateTime date;
+  final String? note;
+  final String? skipReason;
+  ActivityType type;
+  CompletionsStatus status;
+  final DateTime? completedAt;
+  Map<String, dynamic>? activityDetails;
+
+  ActivityRecord({
+    required this.date,
+    required this.status,
+    required this.type,
+    this.note,
+    this.skipReason,
+    this.completedAt,
+    this.activityDetails,
+  });
+
+  ActivityRecord copyWith();
+  Map<String, dynamic> toMap();
+
+  @override
+  List<Object?> get props => [
+    date,
+    status,
+    note,
+    skipReason,
+    type,
+    completedAt,
+    activityDetails,
+  ];
+}
