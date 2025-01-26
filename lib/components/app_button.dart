1cbde582c90f9fbf291ce2b195301fdc4913815c
Lewis <lewisnuikweh@gmail.com>
2025-01-26 21:52:14 +0100

    ui: update app bar and button components

--- lib/components/app_button.dart
+++ lib/components/app_button.dart
@@ -1,28 +1,36 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 
-import '../../core/core.dart';
+import '../../../core/core.dart';
+import 'app_icon.dart';
 
 class AppButton extends StatelessWidget {
-  const AppButton(
-      {super.key,
-      required this.onPressed,
-      required this.label,
-      this.color,
-      this.backgroundColor,
-      this.iconPath,
-      this.icon,
-      this.overrideIconColor = true,
-      this.buttonType = ButtonType.primary,
-      this.isChipButton = false,
-      this.isLoading = false});
+  const AppButton({
+    super.key,
+    required this.onPressed,
+    required this.label,
+    this.color,
+    this.backgroundColor,
+    this.iconPath,
+    this.icon,
+    this.trailingIcon,
+    this.trailingIconPath,
+    this.overrideIconColor = false,
+    this.buttonType = ButtonType.primary,
+    this.isChipButton = false,
+    this.isLoading = false,
+  });
+
   final VoidCallback onPressed;
   final String label;
   final ButtonType buttonType;
   final IconData? icon;
+  final IconData? trailingIcon;
   final String? iconPath;
+  final String? trailingIconPath;
   final Color? color;
   final Color? backgroundColor;
   final bool? overrideIconColor;
@@ -32,14 +40,22 @@ class AppButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final bool isDarkMode = theme.brightness == Brightness.dark;
     if (icon != null && iconPath != null) {
       throw ErrorHint(
-          "Cannot set both an icon and an iconPath property simultaneously, consider removing one");
+        "Cannot set both an icon and an iconPath property simultaneously, consider removing one",
+      );
     }
-    // if (overrideIconColor == true && color == null) {
-    //   throw ErrorHint(
-    //       "Must provide a color property if overrideIconColor is set to true");
-    // }
+    if (trailingIcon != null && trailingIconPath != null) {
+      throw ErrorHint(
+        "Cannot set both a trailingIcon and a trailingIconPath property simultaneously, consider removing one",
+      );
+    }
+    if (overrideIconColor == true && color == null) {
+      throw ErrorHint(
+        "Must provide a color property if overrideIconColor is set to true",
+      );
+    }
 
     ButtonStyle style = ElevatedButton.styleFrom(
       backgroundColor: backgroundColor ?? theme.colorScheme.primary,
@@ -51,10 +67,14 @@ class AppButton extends StatelessWidget {
         labelColor = color ?? theme.colorScheme.onPrimary;
 
         style = ElevatedButton.styleFrom(
-          fixedSize: isChipButton! ? const Size.fromHeight(36) : null,
+          padding: EdgeInsets.symmetric(
+            horizontal: isChipButton! ? kPadding16 : kPadding24,
+          ),
+          fixedSize: isChipButton! ? const Size.fromHeight(42) : null,
           alignment: Alignment.center,
-          backgroundColor: backgroundColor ?? theme.colorScheme.primary,
-          foregroundColor: color ?? Colors.white,
+          //backgroundColor: backgroundColor ?? theme.colorScheme.primary,
+          backgroundColor: backgroundColor ??  theme.iconTheme.color,
+          foregroundColor: color ??  theme.colorScheme.onPrimary,
         );
         break;
 
@@ -62,9 +82,12 @@ class AppButton extends StatelessWidget {
         labelColor = color ?? theme.colorScheme.onPrimaryContainer;
 
         style = ElevatedButton.styleFrom(
-          fixedSize: isChipButton! ? const Size.fromHeight(36) : null,
+          padding: EdgeInsets.symmetric(
+            horizontal: isChipButton! ? kPadding16 : kPadding24,
+          ),
+          fixedSize: isChipButton! ? const Size.fromHeight(42) : null,
           backgroundColor:
-              backgroundColor ?? theme.colorScheme.primaryContainer,
+          backgroundColor ?? (isDarkMode? theme.colorScheme.surfaceContainerHigh : theme.colorScheme.primaryContainer),
           foregroundColor: labelColor,
         );
         break;
@@ -73,61 +96,71 @@ class AppButton extends StatelessWidget {
         labelColor = color ?? theme.colorScheme.primary;
 
         style = ElevatedButton.styleFrom(
-          fixedSize: isChipButton! ? const Size.fromHeight(36) : null,
-          backgroundColor: Colors.transparent,
-          foregroundColor: color ?? theme.colorScheme.primary,
-          side: BorderSide(
-            width: 1,
-            color: labelColor,
+          padding: EdgeInsets.symmetric(
+            horizontal: isChipButton! ? kPadding16 : kPadding24,
           ),
+          fixedSize: isChipButton! ? const Size.fromHeight(42) : null,
+          backgroundColor: Colors.transparent,
+         // foregroundColor: color ?? theme.colorScheme.primary,
+          foregroundColor: color ?? theme.iconTheme.color,
+          side: BorderSide(width: 1, color: labelColor),
         );
 
         break;
       case ButtonType.text:
         style = ElevatedButton.styleFrom(
-          fixedSize: isChipButton! ? const Size.fromHeight(36) : null,
+          fixedSize: isChipButton! ? const Size.fromHeight(42) : null,
           backgroundColor: Colors.transparent,
           foregroundColor: color ?? theme.colorScheme.primary,
         );
         labelColor = color ?? theme.colorScheme.primary;
         break;
-
     }
 
     return ElevatedButton(
-      onPressed: isLoading! ? (){} : onPressed,
+      onPressed: isLoading! ? (){
+        log("Button is loading", name: "AppButton");
+      } : onPressed, // Disabling the button when loading
       style: style,
       child: Center(
-        child: isLoading!
+        child:
+        isLoading!
             ? SpinKitThreeBounce(
-                size: kPadding24,
-                color: theme.scaffoldBackgroundColor,
-              )
+          size: kPadding24,
+          color:  labelColor,
+        )
             : Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Visibility(
-                    visible: icon != null || iconPath != null,
-                    child: Row(
-                      children: [
-                        icon != null
-                            ? Icon(icon)
-                            : SvgPicture.asset(iconPath ?? "",
-                                colorFilter: overrideIconColor!
-                                    ? ColorFilter.mode(
-                                        labelColor, BlendMode.srcIn)
-                                    : null),
-                        const Gap(8)
-                      ],
-                    ),
-                  ),
-                  Text(label,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium!
-                          .copyWith(color: labelColor)),
-                ],
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (iconPath != null || icon != null)
+              AppIcon(
+                size: 24,
+                icon: icon,
+                iconPath: iconPath,
+                color: labelColor,
               ),
+            // Gap for the leading icon if visible
+            if (iconPath != null || icon != null)
+              Gap(kPadding8),
+            Text(
+              label,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: labelColor,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+
+            // Gap for the trailing icon if visible
+            if (trailingIconPath != null || trailingIcon != null)
+              Gap(kPadding8),
+            if (trailingIconPath != null || trailingIcon != null)
+              AppIcon(
+                icon: trailingIcon,
+                iconPath: trailingIconPath,
+                color: labelColor,
+              ),
+          ],
+        ),
       ),
     );
   }
