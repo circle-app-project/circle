d0053fd7952b6a13d2f9cfc63a8454cbd3cfd35e
Lewis <lewisnuikweh@gmail.com>
2025-01-26 21:54:25 +0100

    ui: deprecated `meds_type_selector` oin favour of `generic selector`
    - Removed deprecated `MedsTypeSelector` and `MedsTypeItem` widgets.

--- lib/components/selectable_circular_button.dart
+++ lib/components/selectable_circular_button.dart
@@ -1,12 +1,15 @@
+import 'package:circle/components/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 
-import '../../../../../core/core.dart';
+import '../../core/core.dart';
 
 
 ///Todo: Refactor this widget into a more generic form so it can be used elsewhere
+
+@Deprecated("Create a generic selector widget instead")
 class MedsTypeItem extends StatefulWidget {
   const MedsTypeItem({
     super.key,
@@ -45,7 +48,6 @@ class _MedsTypeItemState extends State<MedsTypeItem>
   @override
   void initState() {
     animationController = AnimationController(vsync: this, duration: 200.ms);
-
     _radiusAnimation = Tween<double>(begin: 36, end: 20).animate(
         CurvedAnimation(parent: animationController, curve: Curves.easeInOut));
 
@@ -140,17 +142,17 @@ class _MedsTypeItemState extends State<MedsTypeItem>
         _label = "Creams & Gels";
         break;
 
-      case MedicationType.custom:
-        _backgroundColor = !isDarkMode
-            ? (widget.isSelected
-                ? AppColours.purple90
-                : AppColours.purple95)
-            : AppColours.purple20;
-        _color =
-            !isDarkMode ? theme.colorScheme.primary : AppColours.purple90;
-        _iconPath = "assets/svg/plus.svg";
-        _label = "Custom";
-        break;
+      // case MedicationType.custom:
+      //   _backgroundColor = !isDarkMode
+      //       ? (widget.isSelected
+      //           ? AppColours.purple90
+      //           : AppColours.purple95)
+      //       : AppColours.purple20;
+      //   _color =
+      //       !isDarkMode ? theme.colorScheme.primary : AppColours.purple90;
+      //   _iconPath = "assets/svg/plus.svg";
+      //   _label = "Custom";
+      //   break;
 
       case MedicationType.unknown:
         _backgroundColor = !isDarkMode
@@ -216,3 +218,116 @@ class _MedsTypeItemState extends State<MedsTypeItem>
     );
   }
 }
+
+
+
+
+
+
+class SelectableCircularButton extends StatefulWidget implements SelectableWidget{
+  const SelectableCircularButton({
+    super.key,
+    this.iconPath,
+
+    this.color,
+    this.backgroundColor,
+    this.label,
+    this.icon,
+    required this.onPressed,
+    required this.isItemSelected,
+  });
+  final String? label;
+  final bool isItemSelected;
+  final String? iconPath;
+  final IconData? icon;
+  final Color? backgroundColor;
+  final Color? color;
+  final VoidCallback onPressed;
+
+  @override
+  State<SelectableCircularButton> createState() => _SelectableCircularButtonState();
+
+  @override
+  bool get isSelected => isItemSelected;
+
+  @override
+  VoidCallback get onSelected => onPressed;
+}
+
+class _SelectableCircularButtonState extends State<SelectableCircularButton> with SingleTickerProviderStateMixin{
+  late final AnimationController animationController;
+  late final Animation<double> _radiusAnimation;
+
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this, duration: 200.ms);
+    _radiusAnimation = Tween<double>(begin: 36, end: 20).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.easeInOutQuart));
+
+    super.initState();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AnimatedBuilder(
+          animation: _radiusAnimation,
+          builder: (context, child) {
+            return InkWell(
+              onTapDown: (TapDownDetails tapDownDetails) {
+                animationController.forward();
+              },
+              onTapUp: (TapUpDetails tapUpDetails) {
+                ///Todo: Refactor this to use Spring animations
+                animationController.reverse(from: 1.0);
+              },
+              onTap: widget.onPressed,
+              splashColor: widget.color?.withValues(alpha: .2) ??  theme.colorScheme.primary.withValues(alpha: .2),
+              //splashColor: Colors.transparent,
+              splashFactory: InkSparkle.splashFactory,
+              borderRadius: BorderRadius.circular(_radiusAnimation.value),
+              child: Ink(
+                height: 72,
+                width: 72,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(_radiusAnimation.value),
+                  color: widget.isItemSelected? theme.colorScheme.primary: widget.backgroundColor ?? theme.colorScheme.surfaceContainer,
+                ),
+                child: Center(
+                  child:  AppIcon(
+                    size: 24,
+                    color: widget.color ?? (widget.isSelected? Colors.white : theme.colorScheme.onSurface),
+                    iconPath: widget.iconPath,
+                    icon: widget.icon,
+                  )
+                ),
+              ),
+            );
+          },
+        ),
+        const Gap(8),
+        if(widget.label !=null)
+        Text(
+          widget.label!,
+          textAlign: TextAlign.center,
+          style: theme.textTheme.bodyMedium!.copyWith(
+              color: theme.colorScheme.onSurface.withValues(alpha: .7)),
+        )
+      ],
+    );
+  }
+}
+
