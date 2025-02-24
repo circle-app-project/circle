d018a4db1146017052ab9f3a432061187d276b6b
Lewis <lewisnuikweh@gmail.com>
2025-02-24 13:56:37 +0100

    meds: made add/edit meds screen to allow for editing meds
    Refactored the add/edit meds screen to handle editing existing medication, including pre-filling fields, updating medication data, and handling date pickers, and implemented form validation.

--- lib/components/app_chip.dart
+++ lib/components/app_chip.dart
@@ -1,3 +1,4 @@
+import 'package:circle/components/app_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
 
@@ -13,6 +14,7 @@ class AppChip extends StatefulWidget {
     this.backgroundColor,
     this.icon,
     this.iconPath,
+    this.padding,
   });
 
   final Function(bool)? onSelected;
@@ -22,41 +24,35 @@ class AppChip extends StatefulWidget {
   final Color? backgroundColor;
   final IconData? icon;
   final String? iconPath;
-
+  final EdgeInsetsGeometry? padding;
 
   @override
   State<AppChip> createState() => _AppChipState();
 }
 
 class _AppChipState extends State<AppChip> {
-  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-      return Chip(
-        onDeleted: widget.onDeleted,
-        deleteIcon: const Icon(HugeIcons.strokeRoundedCancel01),
-        visualDensity: VisualDensity.comfortable,
-        padding: EdgeInsets.only(
-          top: kPadding12,
-          bottom: kPadding12,
-          left: (widget.icon != null || widget.iconPath != null)
-              ? kPadding8
-              : kPadding12,
-          right: kPadding12,
+    return Chip(
+      onDeleted: widget.onDeleted,
+      deleteIcon: const Icon(HugeIcons.strokeRoundedCancel01),
+      visualDensity: VisualDensity.comfortable,
+      padding: widget.padding ?? const EdgeInsets.all(
+   kPadding12,
+      ),
+      backgroundColor:
+          widget.backgroundColor ?? theme.colorScheme.surfaceContainerLow,
+      avatar: AppIcon(icon: widget.icon, iconPath: widget.iconPath, size: 20, color: widget.color,),
+      label: Text(
+        widget.label,
+        style: theme.textTheme.bodyMedium!.copyWith(
+          color:
+          widget.color
         ),
-        backgroundColor: widget.backgroundColor ?? theme.colorScheme.surfaceContainerLow,
-
-        label: Text(
-          widget.label,
-          style: theme.textTheme.bodyMedium!.copyWith(
-              color: isSelected
-                  ? (widget.color ?? theme.colorScheme.primary)
-                  : null),
-        ),
-      );
-    }
-
+      ),
+    );
+  }
 }
