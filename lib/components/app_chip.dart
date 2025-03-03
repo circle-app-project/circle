c6556767a70432dee755b7093975c4d5f816a5b6
Lewis <lewisnuikweh@gmail.com>
2025-03-03 16:08:09 +0100

    ui: improve snack bar and chip

--- lib/components/app_chip.dart
+++ lib/components/app_chip.dart
@@ -1,6 +1,7 @@
 import 'package:circle/components/app_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:springster/springster.dart';
 
 import '../../core/core.dart';
 
@@ -15,6 +16,7 @@ class AppChip extends StatefulWidget {
     this.icon,
     this.iconPath,
     this.padding,
+    this.borderRadius,
   });
 
   final Function(bool)? onSelected;
@@ -25,33 +27,45 @@ class AppChip extends StatefulWidget {
   final IconData? icon;
   final String? iconPath;
   final EdgeInsetsGeometry? padding;
+  final double? borderRadius;
 
   @override
   State<AppChip> createState() => _AppChipState();
 }
 
 class _AppChipState extends State<AppChip> {
-
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
     return Chip(
+      chipAnimationStyle: ChipAnimationStyle(
+        enableAnimation: AnimationStyle(curve: Spring.defaultIOS.toCurve),
+        selectAnimation: AnimationStyle(curve: Spring.defaultIOS.toCurve),
+        deleteDrawerAnimation: AnimationStyle(curve: Spring.defaultIOS.toCurve),
+        avatarDrawerAnimation: AnimationStyle(curve: Spring.defaultIOS.toCurve),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular( widget.borderRadius ?? kPadding24)
+      ),
       onDeleted: widget.onDeleted,
       deleteIcon: const Icon(HugeIcons.strokeRoundedCancel01),
       visualDensity: VisualDensity.comfortable,
-      padding: widget.padding ?? const EdgeInsets.all(
-   kPadding12,
-      ),
+      padding: widget.padding ?? const EdgeInsets.all(kPadding12),
       backgroundColor:
           widget.backgroundColor ?? theme.colorScheme.surfaceContainerLow,
-      avatar: AppIcon(icon: widget.icon, iconPath: widget.iconPath, size: 20, color: widget.color,),
+      avatar:
+          (widget.icon != null || widget.iconPath != null)
+              ? AppIcon(
+                icon: widget.icon,
+                iconPath: widget.iconPath,
+                size: 20,
+                color: widget.color,
+              )
+              : null,
       label: Text(
         widget.label,
-        style: theme.textTheme.bodyMedium!.copyWith(
-          color:
-          widget.color
-        ),
+        style: theme.textTheme.bodyMedium!.copyWith(color: widget.color),
       ),
     );
   }
